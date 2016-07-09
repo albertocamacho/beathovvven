698661beaa046e6d66a54c8671f80a14fe784cbb
albertocamacho <axc8857@rit.edu>
2016-07-09 14:53:05 -0400

    Updated + Stripped

--- client/client.js
+++ client/client.js
@@ -5,13 +5,14 @@ $( document ).ready(function() {
 	var sc = new SoundCloud();
 	var dancer = new Dancer();
 	var initialized = false;
-	var voted = [];
+
 
 
 
 	var kick = dancer.createKick({
 		onKick:function(mag){
-			setRandomBackground();
+			//some function on kick
+			// vibrate
 		}
 	});
 
@@ -28,60 +29,15 @@ $( document ).ready(function() {
 
 
 
-	socket.on('connect', function(){ });	
+	socket.on('connect', function(){ 
+		//when the client connects
+	});	
 
 	socket.on('Update_toClient', function(data){
-		loadMedia(data.gif, data.sc);
+		//loadMedia(data.gif, data.sc);
 	});
 
 
-	socket.on('Giphy_Suggestion_toClient', function(suggestionsString){
-		$('#gif-suggestions').empty();
-		var suggestionsObj = $.parseJSON(suggestionsString); 
-		var suggestions =  $.map(suggestionsObj, function(el) { return el; });
-
-		for(var i = 0; i < suggestions.length; i++){
-			var suggestion = suggestions[i].name;
-			var votes = suggestions[i].votes;
-
-			createGiphySuggestion(suggestion, votes)
-		}
-	}); 
-
-	function createGiphySuggestion(suggestionName, suggestionVotes){
-		var name = suggestionName;
-        var votes = suggestionVotes;
-  
-        var a = document.createElement('a');
-        a.className = 'suggestion';
-        a.addEventListener("click", sendGiphySuggestion);
-
-		$(a).append('<i class="fa fa-thumbs-o-up" aria-hidden="true"></i> ');
-        $(a).append('<span class = "gif-suggestion-vote">' + votes + '</span');
-        $(a).append('<span class = "gif-suggestion-content"> ' + name + ' </span>');
-
-        document.getElementById('gif-suggestions').appendChild(a);
-	}
-
-
-
-	function sendGiphySuggestion(){
-		var suggestion_votes = $(this).find('.gif-suggestion-vote');
-		suggestion_votes = parseInt(suggestion_votes);
-		var suggestion_name = $(this).find('.gif-suggestion-content').html();
-		suggestion_name = suggestion_name.slice(1, -1);
-
-		$(this).className += voted;
-		socket.emit('Giphy_Suggestion_toServer', { suggestion: suggestion_name, votes: suggestion_votes }); 
-	}
-
-	function sendSCSuggestion(){
-		var suggestion_votes = $(this).find('.sc-suggestion-vote').html();
-		suggestion_votes = parseInt(suggestion_votes);
-		var suggestion_name = $(this).find('.sc-suggestion-content').text();
-		suggestion_name = suggestion_name.slice(1, -1);
-		socket.emit('SC_Suggestion_toServer', { suggestion: suggestion_name, votes: suggestion_votes } );
-	}
 
 	var checkLoadedInterval = setInterval(function(){
 		var loaded = dancer.isLoaded();
@@ -95,31 +51,6 @@ $( document ).ready(function() {
 
 
 
-
-
-/*
-    function createSuggestionInput(suggestionClass){
-
-    	var a = document.createElement('a');
-
-        a.addEventListener('keypress', function(e){
-            if (!e) e = window.event;
-            var keyCode = e.keyCode || e.which;
-            if (keyCode == '13'){
-                var a = document.getElementById('gif-suggestion-input').value;
-                socket.emit('New_Giphy_Suggestion_toServer', { suggestion: s, votes: 1 } ); 
-                return false;
-            }
-        }, false);
-
-        
-
-        $(li).append('<input class="menu-input" id = "gif-suggestion-input" type="text" placeholder="suggest">');
-        document.getElementById('gif-suggestion-list').appendChild(li);
-
-    }
-
-    */
 });
 
 
